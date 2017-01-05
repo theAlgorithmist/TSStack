f0407f7445bd2bfd29bc98c0504b90e4dd08c25f
Jim Armstrong <theAlgorithmist@gmail.com>
2017-01-05 14:44:06 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Typescript Math Toolkit Stack
+
+This is the alpha release of the Typescript Math Toolkit Stack.  As I have mentioned in other projects, the TSMT Data Structures library is intended to provided a necessary set of structures to support algorithms in the toolkit and corresponding demos.  I am placing the structures one-at-a-time into the public domain for purposes of additional testing and API feedback.
+
+A stack is a pretty simple structure to code, so you won't find a lot to this other than the TSMT Stack is templatized and supports both FIFO and LIFO access to stack elements via pop().
+
+As an aside, this is the last repository that I will use Jasmine as the test framework.  It seems most devs are familiar with Mocha/Chai, which is what you will see for future repositories.
+
+
+Author:  Jim Armstrong - [The Algorithmist]
+
+@algorithmist
+
+theAlgorithmist [at] gmail [dot] com
+
+Typescript: 2.0.0
+
+Version: 1.0.0
+
+
+## Installation
+
+Installation involves all the usual suspects
+
+  - npm and gulp installed globally
+  - Clone the repository
+  - npm install
+  - get coffee (this is the most important step)
+
+
+### Building and Running the unit tests
+
+1. gulp compile
+
+2. gulp serve
+
+The test suite is in Jasmine (but this is the last one).  The stack is in the _src_ folder. 
+
+
+### Using the stack
+
+A stack may be created directly from an existing array of the appropriate type or elements may be pushed one-at-a-time onto the stack.
+
+
+```
+let stack: TSMT$Stack<number> = new TSMT$Stack<number>();
+let a:Array<number> = [1, 2, 3, 4, 5, 6];
+stack.fromArray(a);
+```
+
+or
+
+```
+let stack: TSMT$Stack<number> = new TSMT$Stack<number>();
+
+ stack.push(1);
+ stack.push(2);
+ stack.push(3);
+```
+
+use the _access_ mutator to change between FIFO and LIFO for accessing successive stack items via _pop_.  The next element to be removed may be examined without removal via _peek_.
+
+The stack may be cleared or reversed, and a method exists to return a copy of the stack as an array of items of appropriate type. 
+
+Pretty much all the usual suspects, blah, blah.  Refer to the specs for additional usage examples.
+
+
+License
+----
+
+Apache 2.0
+
+**Free Software? Yeah, Homey plays that**
+
+[//]: # (kudos http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+[The Algorithmist]: <http://algorithmist.net>
---
+++ src/Stack.ts
@@ -0,0 +1,181 @@
+/** 
+ * Copyright 2016 Jim Armstrong (www.algorithmist.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Typescript Math Toolkit: Minimal implementation of a stack of generic items.  Supports FIFO and LIFO insertion/removal.
+ *
+ * @author Jim Armstrong (www.algorithmist.net)
+ * 
+ * @version 1.0
+ */
+
+ export enum STACK_TYPE 
+ {
+	 'FIFO',
+   'LIFO'
+ }
+
+ export class TSMT$Stack<T>
+ {
+   protected _stack: Array<T>;                // The actual stack
+   protected _access: number;                 // how data is accessed from the stack, i.e. FIFO or LIFO
+   
+/**
+ * Construct a new Stack of the supplied type
+ * 
+ * @return Nothing Constructs an array of the supplied data type and sets acccess to FIFO 
+ */
+  constructor( )
+  {
+    this._stack  = new Array<T>();
+    this._access = STACK_TYPE.FIFO;
+  }
+
+  /**
+	 * Access the length or size of the stack
+	 * 
+	 * @return number - stack length
+	 */
+	 public get length(): number
+	 {
+	   return this._stack.length;
+   }
+
+  /**
+	 * Access the current stack access mode
+	 * 
+	 * @return number - STACK_TYPE.FIFO or STACK_TYPE.LIFO
+	 */
+	 public get access(): number
+	 {
+	   return this._access;
+   }
+
+  /**
+	 * Assign the current stack access mode
+	 * 
+	 * @param mode: number - STACK_TYPE.FIFO or STACK_TYPE.LIFO
+   * 
+   * @teturn Nothing - Assigns the stack access mode to FIFO or LIFO, provided the input is valid
+	 */
+	 public set access(mode: number)
+	 {
+	   this._access = (mode == STACK_TYPE.FIFO || mode == STACK_TYPE.LIFO) ? mode : this._access;
+   }
+
+  /**
+   * Initialize the stack from an existing array
+   * 
+   * @param items: Array<T> Array of stack items
+   * 
+   * @return Nothing - The internal stack is updated to be a copy of the input array
+   */
+   public fromArray(items: Array<T>): void
+   {
+     if (items && items.length && items.length > 0)
+       this._stack = items.slice();
+   }
+
+ /**
+   * Return the current stack as an an array
+   * 
+   * @return Array<T> - A copy of the current stack; the current stack is not modified.
+   */
+   public toArray(): Array<T>
+   {
+     return this._stack.slice();
+   }
+
+  /**
+   * Add an item to the stack
+   *
+   * @param item: T Data item of type T
+   *
+   * @return Nothing - A non-null item is pushed onto the stack - no runtime type guard is currently applied, but this may change in the future
+   */
+   public push(item: T): void
+   {
+     if (item)
+       this._stack.push(item);
+   }
+
+  /**
+   * Peek at the next item to be removed from the stack without removing it
+   *
+   * @return item: T - Direct reference to the next item that will be removed from the stack; take care not to mutate the item.  Returns null for an empty stack.
+   */
+   public peek(): T
+   {
+     if (this._stack.length == 0)
+       return null;
+
+     switch (this._access)
+     {
+       case STACK_TYPE.FIFO:
+         return this._stack[0];
+
+       case STACK_TYPE.LIFO:
+         return this._stack[this._stack.length-1];
+     }
+
+     return null;
+   }
+
+  /**
+   * Pop the next item off the stack
+   *
+   * @return item: T - Direct reference to the next stack item, based on current access type (FIFO or LIFO), or null for an empty stack.  The item is permanently removed from the stack.
+   */
+   public pop(): T
+   {
+     if (this._stack.length == 0)
+       return null;
+
+     switch (this._access)
+     {
+       case STACK_TYPE.FIFO:
+         return this._stack.shift();
+
+       case STACK_TYPE.LIFO:
+         return this._stack.pop();
+     }
+
+     return null;
+   }
+
+  /**
+   * Reverse the stack
+   *
+   * @return Nothing There is no action for an empty stack; otherwise, the order of the stack is reversed.  This does not, however, affect future insertions and deletions from the stack.
+   */
+   public revsere(): void
+   {
+     if (this._stack.length == 0)
+       return;
+
+     this._stack.reverse();
+   }
+
+  /**
+	 * Clear stack
+	 * 
+	 * @return Nothing - stack is emptied and is ready for new data.  Access mode remains unchanged.
+	 */
+	 public clear(): void
+	 {
+	   this._stack.length = 0;
+   }
+ }
